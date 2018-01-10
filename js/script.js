89546364adc82bcad84e59cc631b4b02437b22ed
Oscar Ramirez <oramirez2020@bklawtech.com>
2018-01-10 02:49:25 +0000

    start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*global $*/
 // Pig Latin takes the first consonant (or consonant cluster) of an English 
 // word, moves it to the end of the word and suffixes an ay, or if a word
 // begins with a vowel you just add ay to the end. For example, pig becomes
@@ -6,8 +7,9 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
-
+  $("#btn").click(function(){
+  	$("#result").html($("#info").val() +"ay")
+  });
 
 
 });
@@ -16,12 +18,6 @@ $( document ).ready(function() {
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
 
-
-
-
-
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
-
-
